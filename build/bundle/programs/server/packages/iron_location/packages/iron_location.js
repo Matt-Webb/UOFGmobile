c4a008323ff2afcb08dd2b6c0dcc92cf5555d1a9
Matthew Webb <rybka.webb@gmail.com>
2015-10-27 17:24:35 +0000

    initial commit

---
+++ build/bundle/programs/server/packages/iron_location/packages/iron_location.js
@@ -0,0 +1,16 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var _ = Package.underscore._;
+var Tracker = Package.tracker.Tracker;
+var Deps = Package.tracker.Deps;
+var Iron = Package['iron:core'].Iron;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['iron:location'] = {};
+
+})();
